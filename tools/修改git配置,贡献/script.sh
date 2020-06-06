d4d4e1210c6ce894116f7b20df1749513b235ffe
zhenqiufu <fuzhenqiu0810@gmail.com>
2020-06-06 14:20:45 +0800

    add 修改git邮箱错误

---
+++ tools/修改git配置,贡献/script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+git filter-branch --env-filter '
+OLD_EMAIL="fuzheqniu0810@gmail.com"
+CORRECT_NAME="zhenqiufu"
+CORRECT_EMAIL="fuzhenqiu0810@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
